ca22d28e3d469263531479a29959eeed182cd85d
funkabo <funkabo@protonmail.com>
2021-10-08 16:22:34 +0200

    Third Commit - Exercise 1 Test

--- week-1/exercise-1/src/test/java/se/lexicon/AppTest.java
+++ week-1/exercise-1/src/test/java/se/lexicon/AppTest.java
@@ -1,20 +1,29 @@
 package se.lexicon;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import se.lexicon.entity.Student;
+
 
 /**
- * Unit test for simple App.
+ * ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+ * ┃                       Exercise 1 Test                         ┃
+ * ┃   unit test: App                                              ┃
+ * ┃     version: 1.0                                              ┃
+ * ┃      author: Federico Sanders <federico.sanders@hotmail.com>  ┃
+ * ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
  */
+
 public class AppTest 
 {
     /**
-     * Rigorous Test :-)
+     * Should be Equal
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldBeEqual() {
+        Student student = new Student.StudentBuilder().withLastName("Sanders").build();
+        assertEquals("Sanders", student.getLastName());
     }
 }
