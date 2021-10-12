8e7b7b32a5d24d3e41341565b0fa96f9afea4154
funkabo <funkabo@protonmail.com>
2021-10-12 14:14:27 +0200

    Second Commit - Added comments to Exercise 1

--- week-2/exercise-1/src/main/java/se/lexicon/App.java
+++ week-2/exercise-1/src/main/java/se/lexicon/App.java
@@ -1,13 +1,16 @@
 package se.lexicon;
 
 /**
- * Hello world!
- *
+ * ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+ * ┃                            Exercise 1                        ┃
+ * ┃      title: Calculator                                       ┃
+ * ┃    version: 1.0                                              ┃
+ * ┃     author: Federico Sanders <federico.sanders@hotmail.com>  ┃
+ * ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+
+public class App {
+    public static void main( String[] args ){
         System.out.println( "Hello World!" );
     }
 }
--- week-2/exercise-1/src/main/java/se/lexicon/core/Calculator.java
+++ week-2/exercise-1/src/main/java/se/lexicon/core/Calculator.java
@@ -1,2 +1,13 @@
-package se.lexicon.core;public class Calculator {
+package se.lexicon.core;
+
+/**
+ * ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+ * ┃                            Exercise 1                        ┃
+ * ┃      title: Calculator                                       ┃
+ * ┃    version: 1.0                                              ┃
+ * ┃     author: Federico Sanders <federico.sanders@hotmail.com>  ┃
+ * ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+ */
+
+public class Calculator {
 }
